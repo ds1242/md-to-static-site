12e9a9d70c955163f29042f23306b2b817f8341b
David Shaw <dshaw11@utk.edu>
2024-03-18 14:44:04 -0400

    still working on adding tests to check how valid parentnode is

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,14 +32,27 @@ class LeafNode(HTMLNode):
         if self.tag == None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
         
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag, value, children, props=None):
-        super().__init__(tag, None, children, props)
-        
-    def to_html(self):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
         if not self.tag:
             raise ValueError('tag required')
         if not self.children:
             raise ValueError('children values required')
+        
+    def to_html(self):
+        result = ""
+        if len(self.children) == 0:
+            return result
+        for child in self.children:
+            result += child.to_html()
+        return f"<{self.tag}>{result}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
---
+++ src/test_parentnode.py
@@ -0,0 +1,32 @@
+import unittest
+from htmlnode import ParentNode,LeafNode
+
+class TestParentNode(unittest.TestCase):
+    def test_parent(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+        )
+    def test_no_tag(self):
+        node = ParentNode(
+            None,
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+            {"class": "greeting", "href": "https://boot.dev"}
+        )
+        self.assertRaises(ValueError('tag required'), node.to_html())
+
+
