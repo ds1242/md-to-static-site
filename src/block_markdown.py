28f01879f0f1d39c5c0085389882d3703ffe9506
David Shaw <dshaw11@utk.edu>
2024-03-20 11:13:46 -0400

    finished markdown to blocks, a lot easier than the inline so far

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -3,8 +3,11 @@ def markdown_to_blocks(markdown):
     output = []
     split_block = markdown.split("\n\n")
     for block in split_block:
-        block = block.strip(" ")
-        output.append(block)
+        if block == "":
+            continue
+        else:
+            block = block.strip()
+            output.append(block)
     return output
 
 
