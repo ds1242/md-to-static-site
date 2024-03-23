624ef765a06e189096783c2dadc9db4589fec368
David Shaw <dshaw11@utk.edu>
2024-03-23 14:23:08 -0400

    setup extract title function that gets the title from the markdown function

--- src/extract_title.py
+++ src/extract_title.py
@@ -1,12 +1,4 @@
 
-
-
 def extract_title(markdown):
     split_file = markdown.split("\n")
-    print(split_file[0])
-
-
-
-f = open('./content/index.md', 'r')
-markdown_file = f.read()
-extract_title(markdown_file)
+    return split_file[0].strip('# ')
