93f9ae13ba09df1aefb03b41d2a24165c2f8b9b5
David Shaw <dshaw11@utk.edu>
2024-03-23 13:57:27 -0400

    setup the recursive function to copy files from a source to a destination and keeping any subdirectories

--- src/copy_static.py
+++ src/copy_static.py
@@ -1,38 +1,35 @@
 import os
 import shutil
 
-def create_file_path(path, output_list):
+def copy_files(source_path, destination):
     path_list = []
-    if not os.path.exists(path):
+    check_destination(destination)    
+
+    if not os.path.exists(source_path):
         raise Exception('not a valid path')
     
-    if os.path.isfile(path):
-        output_list.append(path)
+    if os.path.isfile(source_path):
+        shutil.copy(source_path, destination)
     else:
-        path_list = os.listdir(path=path)
+        path_list = os.listdir(path=source_path)
 
     for current_path in path_list:
-        curr_path = os.path.join(path, current_path)
+        curr_path = os.path.join(source_path, current_path)
         if os.path.isfile(curr_path):
-            output_list.append(curr_path)
+            shutil.copy(curr_path, destination)
         else:
-            create_file_path(curr_path, output_list)
-    return output_list
+            destination = destination + curr_path[len(source_path):]            
+            copy_files(curr_path, destination)
+    
 
 
-def copy_files(path_list, destination):
-    destination_path_list = []
+
+
+
+def check_destination(destination):
     if os.path.exists(destination):
         shutil.rmtree(destination)
     os.mkdir(destination)
-    if len(path_list) == 0:
-        raise Exception('no source available')
-    
-    for path in path_list:
-        destination_path_list.append(shutil.copy(path, destination))
-    return destination_path_list
-
-
 
     
         
--- src/test_copystatic.py
+++ src/test_copystatic.py
@@ -1,8 +1,6 @@
 import unittest
 from copy_static import (
-    create_file_path,
     copy_files,
-    trim_path_list
 )
 
 
@@ -11,28 +9,11 @@ class TestCopyStaticFiles(unittest.TestCase):
     def test_create_file_path(self):
         output = []
         start_path = './static'
-        output = create_file_path(start_path, output)
-        self.assertEqual(
-            output,
-            ['./static/index.css', './static/images/rivendell.png']
-        )
-
-    def test_file_path_no_dir(self):
-        output = []
-        start_path = './static/images/rivendell.png'
-        output = create_file_path(start_path, output)
-        self.assertEqual(
-            output,
-            ['./static/images/rivendell.png']
-        )
-
-    def test_copy_file(self):
-        destination_list = []
-        list_of_files = []
-        list_of_files = create_file_path('./static', list_of_files)
-        print(list_of_files)
         destination = './public'
-        destination_list = copy_files(list_of_files, destination)
+        output = copy_files(start_path, output, destination)
+        self.assertEqual(
+            output,
+            ['./public/index.css', './public/images/rivendell.png']
+        )
 
-        print(destination_list)
         
