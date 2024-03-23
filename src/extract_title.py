681f563fd5b84ffcdd4279e5a07ed21a2ae06e88
David Shaw <dshaw11@utk.edu>
2024-03-23 14:20:57 -0400

    started to figure out how to extract the title of a markdown file

--- src/extract_title.py
+++ src/extract_title.py
@@ -2,4 +2,11 @@
 
 
 def extract_title(markdown):
-    pass
+    split_file = markdown.split("\n")
+    print(split_file[0])
+
+
+
+f = open('./content/index.md', 'r')
+markdown_file = f.read()
+extract_title(markdown_file)
