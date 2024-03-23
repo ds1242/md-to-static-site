422012cfdc5b78b90cb981cf8c3c5ac06925de07
David Shaw <dshaw11@utk.edu>
2024-03-23 14:01:46 -0400

    removed unused output list

--- src/copy_static.py
+++ src/copy_static.py
@@ -2,12 +2,11 @@ import os
 import shutil
 
 def copy_files(source_path, destination):
-    path_list = []
     check_destination(destination)    
 
     if not os.path.exists(source_path):
         raise Exception('not a valid path')
-        
+
     for current_path in os.listdir(source_path):
         curr_path = os.path.join(source_path, current_path)
         if os.path.isfile(curr_path):
