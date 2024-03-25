5dafbf39b9716c252525ab62634fddaed7b36c1a
David Shaw <dshaw11@utk.edu>
2024-03-25 08:49:55 -0400

    changes to ultimately fix the textnode

---
+++ src/generate_page.py
@@ -0,0 +1,22 @@
+from extract_title import extract_title
+from block_markdown import markdown_to_html_node
+import os
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page {from_path} to {dest_path} using {template_path}")
+    with open(from_path, 'r') as original_markdown:
+        content = original_markdown.read()
+        title = extract_title(content)        
+        content = markdown_to_html_node(content)
+        original_markdown.close()
+
+        
+    with open(template_path, "r") as template:
+        template_path_data = template.read()
+        template_path_data = template_path_data.replace("{{ Title }}", title)
+        template_path_data = template_path_data.replace("{{ Content }}", content.to_html())
+        # print(template_path_data)
+    if not os.path.exists(dest_path):
+        os.makedirs(os.path.dirname(dest_path))
+
+    os.path.dirname(dest_path).write(template_path_data)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from textnode import TextNode
 from copy_static import copy_files
+from generate_page import generate_page
 
 def main():
     test_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    source_path = './static'
-    destination = './public'
-    copy_files(source_path, destination)
+    # source_path = './static'
+    # destination = './public'
+    # copy_files(source_path, destination)
+    source = './content/index.md'
+    template = './template.html'
+    destination = './public/index.html'
+    generate_page(source, template, destination)
 
 main()
